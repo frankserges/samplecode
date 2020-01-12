03976348e570c84172efe8b6dbb21e2b7a9c6f9d
frank Ndjekoua <franckndjekoua@yahoo.fr>
2020-01-12 10:41:14 +0100

    v1

---
+++ blinkingled/Debug/blink.d
@@ -0,0 +1,17 @@
+# FIXED
+
+blink.obj: ../blink.c
+blink.obj: C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430.h
+blink.obj: C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430fr5994.h
+blink.obj: C:/ti/ccs901/ccs/ccs_base/msp430/include/in430.h
+blink.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics.h
+blink.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics_legacy_undefs.h
+blink.obj: C:/ti/ccs901/ccs/ccs_base/msp430/include/legacy.h
+
+../blink.c: 
+C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430fr5994.h: 
+C:/ti/ccs901/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccs901/ccs/ccs_base/msp430/include/legacy.h: 
